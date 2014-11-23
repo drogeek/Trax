cfbe6109d2b28e45e53b7ad00b56723f0f94fbcb
Dylan Werner-Meier <dylan-werner-meier@etud.univ-tln.fr>
2014-11-23 15:45:32 +0100

    Ajout d'une MapFragment de test. Le code fonctionne. Pour que la map s'affiche, il faut aller dans Build -> Generate signed APK ; puis installer à la main via adb install l'apk généré.

--- src/com/trax/MainMenu.java
+++ src/com/trax/MainMenu.java
@@ -1,7 +1,9 @@
 package com.trax;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends Activity {
     /**
@@ -11,5 +13,9 @@ public class MainMenu extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void show_map(View v){
+        startActivity(new Intent(this, MapActivity.class));
     }
 }
---
+++ src/com/trax/MapActivity.java
@@ -0,0 +1,15 @@
+package com.trax;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by unautre on 23/11/14.
+ */
+public class MapActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_layout);
+    }
+}
