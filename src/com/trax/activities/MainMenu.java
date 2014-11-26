080b4a9c751491f5bcdcce1d68aad7cfff23ad05
Dylan Werner-Meier <dylan-werner-meier@etud.univ-tln.fr>
2014-11-26 15:34:14 +0100

    Gestion des erreurs

--- src/com/trax/activities/MainMenu.java
+++ src/com/trax/activities/MainMenu.java
@@ -3,8 +3,10 @@ package com.trax.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import com.trax.R;
+import com.trax.errors.AlreadyLaunchedSessionException;
 import com.trax.modes.Session;
 import com.trax.modes.SessionEnregistrement;
 import com.trax.modes.SessionItinerant;
@@ -23,13 +25,21 @@ public class MainMenu extends Activity {
 
     public void launch_record(View v){
         /* TODO ! */
-        new SessionEnregistrement();
-        startActivity(new Intent(this, SelectionContacts.class));
+        try {
+            new SessionEnregistrement();
+            startActivity(new Intent(this, SelectionContacts.class));
+        } catch (AlreadyLaunchedSessionException e) {
+            Log.e("TRAX", e.toString());
+        }
     }
 
     public void launch_pathway(View v){
         /* TODO ! */
-        new SessionItinerant();
-        startActivity(new Intent(this, SelectionItineraire.class));
+        try {
+            new SessionItinerant();
+            startActivity(new Intent(this, SelectionItineraire.class));
+        } catch (AlreadyLaunchedSessionException e) {
+            Log.e("TRAX", e.toString());
+        }
     }
 }
